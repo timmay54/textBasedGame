d25d3e5b53d7ab03cebecdfb4f2185ac920189e5
timmay54 <timothymardesen@yahoo.com>
2018-04-03 11:16:46 -0500

    more dictionaries, better implementation, and adding a new broken loop! (

--- textBasedGame.py
+++ textBasedGame.py
@@ -8,49 +8,85 @@ This game focuses on choosing wich tools are the handiest in the wrong run.
 CONTROLS:
 
 
+
+
+
+
+
+TO DO -
+
+Needs to be connected to the dictionary for the players' items.
+
 """
 
-"""GAME BLOCK        """
-def game_main()
-	startGame = true
+"""   GAME BLOCK    """
+def game_main():
+	startGame = True
 	lives = 3
-	itemMAX = 4
-	repsone=input("You are standing in a kitchen, dazed and confused.")
-	if (response=="smoke weed")
-		print("holy fuck you just won the universe")
-
-	elif(response=="help")
-		print("You can: \nDrink water\nWalk outside\nLook in fridge\nCall someone on your phone.")
-	else
-		print("You need help? Probaby should ask for some...")
-
-
-dict_places {
+	itemMAX = len(dict_player_items) 
+	response = input("You are standing in a kitchen, dazed and confused.")
 	
-	"kitchen" : 1 ,
-	"bathroom" : 2 ,
-	"living room" : 3 ,
-	"stairs2" : 4 ,
-	"stairs3" : 5 , 
-	"attic" : 6, 
-	"tiny pink room" : 7 ,
-	"angry man's room" : 8 ,
-	"business man's room" : 9 , 
-	"the pig man's room" : 10 , 
-	"the best man's room" : 11 ,
-	"front door" : 12 
+	while (response != "quit"):
+		try:
+			if (response == "smoke weed"):
+				print("holy fuck you just won the universe")
+
+			elif(response=="help"):
+				print("You can: \nDrink water\nWalk outside\nLook in fridge\nCall someone on your phone.")
+			else:
+				print("You need help? Probaby should ask for some...")
+		except Exception as e:
+			print("Fatal error has occurred!\a")
+		else:
+			print ("You should ask for 'help'.")
+
+
+
+dict_places = {
+	
+	1  : "kitchen"        ,
+	2  : "bathroom"       ,
+	3  : "living room"    ,
+	4  : "stairs2"         ,
+	5  : "stairs3"          , 
+	6  : "attic"             , 
+	7  : "tiny pink room"     ,
+	8  : "angry man's room"    ,
+	9  : "business man's room" , 
+	10 : "the pig man's room"  , 
+	11 : "the best man's room" ,
+	12 : "front door"   
 }
 
-dict_startmenu {
-	"Start" : 1 ,
-	"How to play" : 2 ,
-	"Puss the fuck out" : 3 ,
+dict_kitchen = {
+	
+	"fridge"     : "You see some old ham, beer in the drawers, and jello shots." ,
+	"cupboards"  : "Ramen, egg noodles, and jarred stuff from the business man." ,
+	"Closet"     : "a red cooler, backpack, broom, and extra lightbulbs." 
+
+
+
+}
+
+dict_player_items = {
+	
+
+}
+
+dict_startmenu = {
+	1 : game_main()          ,
+	2 : print ("How to play")        ,
+	3 : "Puss the fuck out"   
 }
 
 
 """This is the start menu of the game. This block runs first."""
 
 print ("Welcome to _________!")
-ans = input("1. Start\n2.How to play\n3.Puss the fuck out")
-if (ans==1)
-	game()
+ans = input("1.Start\n2.How to play\n3.Puss the fuck out\n\n")
+try:
+	dict_startmenu[ans]
+except Exception as e:
+	print("fatal error in start block!")
+else:
+	print("else block in start block has been reached.")
