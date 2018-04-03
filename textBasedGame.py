9e99693d101b4f17a4385a1ed06748d966e5dd48
timmay54 <timothymardesen@yahoo.com>
2018-04-02 21:53:04 -0500

    This is a text based game (obviously) that uses dictionary keys as a case-switch object.

---
+++ textBasedGame.py
@@ -0,0 +1,30 @@
+"""
+Text based game written in python
+
+began writting on 3/31/2018 @ 12:14 AM 
+
+This game focuses on choosing wich tools are the handiest in the wrong run.
+
+CONTROLS:
+
+
+"""
+
+"""GAME BLOCK        """
+def game_main()
+	startGame = true
+	lives = 3
+	itemMAX = 4
+	repsone=input("You are standing in a kitchen, dazed and confused.")
+	if (response=="smoke weed")
+		print("holy fuck you just won the universe")
+
+	elif(response=="help")
+		print("You can: \nDrink water\nWalk outside\nLook in fridge\nCall someone on your phone.")
+	else
+		print("You need help? Probaby should ask for some...")
+
+print ("Welcome to _________!")
+ans = input("1. Start\n2.How to play\n3.Puss the fuck out")
+if (ans==1)
+	game()
