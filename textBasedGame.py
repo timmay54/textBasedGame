099a347c6195be48eaac6deead4bafd27276d7b7
timmay54 <timothymardesen@yahoo.com>
2018-05-18 10:51:44 -0500

    Pushing from laptop, ive switched print statements to C style formatting.

--- textBasedGame.py
+++ textBasedGame.py
@@ -19,19 +19,17 @@ these things should probably be moved to the readme...
 
 
 
-<<<<<<< HEAD
 if windows, you need to clear window with os.system('cls')
 if terminal, clear with os.system('clear')
+find a pattern that works for clearing teriminal by setting a variable based on platform.os output
 
 """
-=======
 import platform, time, os
 
 playerScore = 0
 startGame = True
 endGame = False
 lives = 3
->>>>>>> e1632b0e57000cab01203a1511d9775c8a920b1c
 
 import platform #this is for determining the computer running the file
 import time
@@ -85,37 +83,35 @@ def endGameStats():
 	#create file (if file doesn't already exist)
 	#scan to see if the user's current time is better than any of the top ten
 	#if yes, then replace user's time with that number, and shift all worse times down the list
+
+	#could add a variable below that determines what gets printed, between you suck, you have completed the game, and something awesome.
 	print('you lost, you suck!')
 
 """   GAME BLOCK    """
 
 def game_main():
-<<<<<<< HEAD
-	startGame = True
-	lives = 3
-	itemMAX = len(dict_player_items)
-	response = input("You are standing in a kitchen, dazed and confused.")
-=======
 
 	itemMAX = len(dict_player_items)
 	response = "boobs"
->>>>>>> e1632b0e57000cab01203a1511d9775c8a920b1c
 	while (response != "quit"):
 		try:
 			response = input("You are standing in a kitchen, dazed and confused.\n")
-			
+
 			if (response == "smoke weed"):
 				print("holy fuck you just won the universe\n")
 				#play audio OOOHHHH FUCCKKK mlg
 				"""for num in range(0,10000):
 					playerScore = playerScore + 100"""
-				print("Look at your score!!! " and playerScore)
-					
+				print("Look at your score!!! %d", playerScore)
+
 				gameEnd = True
 				startGame = False #Probably dont need both of these...
 				break
 
-			elif(response=="help"):
+			elif(response == "living room") or (response == "go to living room"): #use a REGEX here to see if response has the words "living room" in it, which would eliminate a bunch of "or's"
+				print("You are standing in the living room. There is a bag hanging from the ceiling fan.")
+
+			elif(response == "help"):
 				print("You can: \nDrink water\nWalk outside\nLook in fridge\nCall someone on your phone.")
 				enter = input("Press enter to continue")
 
@@ -128,32 +124,22 @@ def game_main():
 		else:
 			print ("You should ask for 'help'.")
 
-
-
-"""This is the start menu of the game. This block runs first."""
-
 def start_menu():
 	print ( platform.platform() )
 	print ( platform.system()   )
 	print ( platform.release()  )
 	print ( platform.version()  )
 	print ("This is determining your computer type and brand")
-	startGame = False
 
 	print ("Welcome to _________!")
 	ans = int(input("1. Start\n2. How to play\n3. Quit\n\n") ) ###had to change this for online purposes LOL
 
-	print ("\nYou have entered " + ans)
+	print ("\nYou have entered", ans)
 
 	try:
 		if (ans == 1 or ans == "1"):
-<<<<<<< HEAD
-			game_main()
-		elif (ans == 2 or ans == "2"):
-=======
 			startGame = True
 		elif (ans == 2):
->>>>>>> e1632b0e57000cab01203a1511d9775c8a920b1c
 			print (dict_startmenu[2])
 		elif (ans == 3 or ans == "3"):
 			print (dict_startmenu[3])
@@ -162,12 +148,7 @@ def start_menu():
 
 	except Exception as e:
 		print("fatal error in start block")
-<<<<<<< HEAD
 
-
-start_menu()
-=======
-	
 """
 Main Block
 """
@@ -180,4 +161,3 @@ if startGame == True:
 	game_main()
 elif (startGame == False) and  (endGame == True):
 	endGameStats()
->>>>>>> e1632b0e57000cab01203a1511d9775c8a920b1c
